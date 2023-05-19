4cb772bcbf99b67ec9dc253335bebb17a8f51abb
alicemirigo92 <alicemirigo92@gmai.com>
2023-05-20 00:56:26 +0300

    Add form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post, counter_cache: :commentscounter
-  after_save :update_comments_counter
+  belongs_to :author, foreign_key: :author_id, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
+  after_save :update_post_comment_counter
 
   private
 
-  def update_comments_counter
+  def update_post_comment_counter
     post.increment!(:commentscounter)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :post, counter_cache: :likescounter
-  after_save :update_likes_counter
+  belongs_to :post
 
-  private
+  after_save :update_likes_counter
 
   def update_likes_counter
     post.increment!(:likescounter)
