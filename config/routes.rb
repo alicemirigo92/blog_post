b94650a19afc0bc2c4b2d8462586c0a650a709c6
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-05-25 15:21:24 +0530

    updated database.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[new create]
