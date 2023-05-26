60953cc5b9803476c01b1ccbf6222743f5320837
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-05-26 18:00:18 +0530

    updated post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,15 @@
    validates :commentscounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
    validates :likescounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
    after_save :update_user_post_counter
+   
    def update_user_post_counter
      author.increment!(:postscounter)
    end
+
    def five_most_recent_comments
      comments.order(created_at: :desc).limit(5)
    end
+
    def decrement_user_post_counter
      author.decrement!(:postscounter)
    end
