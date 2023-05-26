77422b669506c1b35652166a6f8fbced091b6310
alicemirigo92 <alicemirigo92@gmai.com>
2023-05-26 13:41:22 +0300

    Add authorization

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,17 @@
 class Comment < ApplicationRecord
   belongs_to :author, foreign_key: :author_id, class_name: 'User'
   belongs_to :post, class_name: 'Post'
+
   after_save :update_post_comment_counter
+  after_destroy :decrement_post_comment_counter
 
   private
 
   def update_post_comment_counter
     post.increment!(:commentscounter)
+  end
+
+  def decrement_post_comment_counter
+    post.decrement!(:commentscounter)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[new create]
-      resources :likes, only: [:create]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:new, :create, :destroy]
+      resources :likes, only: [:create, :destroy]
     end
   end
-
   devise_scope :user do
     authenticated :user do
       root 'users#index', as: :authenticated_root
