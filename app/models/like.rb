a31644a66df6dd77a310d59ae1beafc8e4b613a1
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-05-26 18:02:37 +0530

    fixed linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
- class Like < ApplicationRecord
-   belongs_to :author, class_name: 'User'
-   belongs_to :post
-   after_save :update_likes_counter
-   def update_likes_counter
-     post.increment!(:likescounter)
-   end
- end
+class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+  after_save :update_likes_counter
+  def update_likes_counter
+    post.increment!(:likescounter)
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,22 +1,22 @@
- class Post < ApplicationRecord
-   belongs_to :author, class_name: 'User'
-   has_many :comments, foreign_key: 'author_id', dependent: :destroy
-   has_many :likes, foreign_key: 'author_id', dependent: :destroy
-   validates :title, presence: true, length: { maximum: 250 }
-   validates :commentscounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
-   validates :likescounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
-   after_save :update_user_post_counter
-   
-   def update_user_post_counter
-     author.increment!(:postscounter)
-   end
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :commentscounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likescounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  after_save :update_user_post_counter
 
-   def five_most_recent_comments
-     comments.order(created_at: :desc).limit(5)
-   end
+  def update_user_post_counter
+    author.increment!(:postscounter)
+  end
 
-   def decrement_user_post_counter
-     author.decrement!(:postscounter)
-   end
-   private :update_user_post_counter, :decrement_user_post_counter
- end
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def decrement_user_post_counter
+    author.decrement!(:postscounter)
+  end
+  private :update_user_post_counter, :decrement_user_post_counter
+end
