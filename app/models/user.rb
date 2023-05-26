cd9001110bf5f21b98822dc9a63827b4429151b3
alicemirigo92 <alicemirigo92@gmai.com>
2023-05-26 19:07:34 +0300

    Add API

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
@@ -12,5 +12,9 @@ class User < ApplicationRecord
 
   def most_recent_posts
     Post.limit(3).order(created_at: :desc).where(author: self)
+  end
+
+  def admin?
+    role == 'admin' ? true : false
   end
 end
